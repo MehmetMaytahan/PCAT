fd0a5353dde5643ef596119bff09519fc1983d49
Mehmet Maytahan <maytahan71@gmail.com>
2023-01-09 21:27:12 +0300

    db baglantısı ve içeriklerin db'den getirilmesi

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 
 const app = express();
 const path = require("path");
 const port = 3000;
 
+const Photo = require("./models/Photo");
+
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
 // MIDDLEWARE
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // ROUTES
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const Photos = await Photo.find({});
+  res.render("index", { Photos });
 });
 
 app.get("/about", (req, res) => {
@@ -24,4 +30,23 @@ app.get("/add", (req, res) => {
   res.render("add");
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.post("/photos", async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect("/");
+});
+
+mongoose.set("strictQuery", false);
+mongoose.connect(
+  "mongodb://127.0.0.1/pcat-test-db",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  err => {
+    if (err) throw err;
+    console.log("Connected to DB");
+    app.listen(port, () =>
+      console.log(`Example app listening on port ${port}!`)
+    );
+  }
+);

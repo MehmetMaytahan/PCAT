1e0511a76bca169a08f445e90e94cbd9e1eabfbf
Mehmet Maytahan <maytahan71@gmail.com>
2023-01-14 17:51:34 +0300

    route yapisi tasarlandi

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ const fs = require("fs");
 const port = 3000;
 
 const Photo = require("./models/Photo");
+const {
+  getAllPhotos,
+  getPhoto,
+  createPhoto,
+  updatePhoto,
+  deletePhoto
+} = require("./controllers/photoControllers");
+const {
+  getAboutPage,
+  getAddPage,
+  getEditPage
+} = require("./controllers/pageContollers");
 
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
@@ -18,116 +30,18 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
-app.use(
-  methodOverride("_method", {
-    methods: ["POST", "GET"]
-  })
-);
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
 // ROUTES
 
-// route to home page
-app.get("/", async (req, res) => {
-  const Photos = await Photo.find({}).sort("-dateCreated");
-  res.render("index", { Photos, page_name: "index" });
-});
-
-// route to about page
-app.get("/about", (req, res) => {
-  res.render("about", { page_name: "about" });
-});
-
-// route to add page
-app.get("/add", (req, res) => {
-  res.render("add", { page_name: "add" });
-});
-
-// add new photo
-app.post("/photos", async (req, res) => {
-  const uploadDir = "public/uploads";
-  let uploadImage = req.files;
-
-  // if there is no uploads folder, create one
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
-
-  // if there is no picture uploaded
-  if (uploadImage === null) {
-    await Photo.create({
-      ...req.body
-    });
-    res.redirect("/");
-  } else {
-    let uploadPath = path.join(
-      __dirname,
-      "public/uploads",
-      uploadImage.image.name
-    );
-
-    uploadImage.image.mv(uploadPath, async () => {
-      await Photo.create({
-        ...req.body,
-        image: `/uploads/${uploadImage.image.name}`
-      });
-    });
-    res.redirect("/");
-  }
-});
-
-// route to get photo detail
-app.get("/photos/:id", async (req, res) => {
-  const photo = await Photo.findById(req.params.id);
-  res.render("photo-detail", { photo, page_name: "photo_detail" });
-});
-
-// route to edit photo
-app.get("/photos/edit/:id", async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  res.render("edit", { photo, page_name: "edit" });
-});
-
-// update photo
-app.put("/photos/:id", async (req, res) => {
-  let uploadedImage = req.files; // you can use req.files.file.mv() to save file
-  const photo = await Photo.findOne({ _id: req.params.id });
-
-  // if there is no picture uploaded
-  if (uploadedImage === null) {
-    photo.title = req.body.title;
-    photo.description = req.body.description;
-    await photo.save();
-    res.redirect(`/photos/${photo._id}`);
-  } else {
-    let uploadPath = path.join(
-      __dirname,
-      "public/uploads",
-      uploadedImage.image.name
-    );
-
-    uploadedImage.image.mv(uploadPath, async () => {
-      photo.title = req.body.title;
-      photo.description = req.body.description;
-      photo.image = `/uploads/${uploadedImage.image.name}`;
-      await photo.save();
-    });
-    if (fs.existsSync(`public/${photo.image}`)) {
-      fs.unlinkSync(`public/${photo.image}`);
-    }
-    res.redirect(`/photos/${photo._id}`);
-  }
-});
-
-// delete photo
-app.delete("/photos/:id", async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-
-  if (fs.existsSync(`public/${photo.image}`)) {
-    fs.unlinkSync(`public/${photo.image}`);
-  }
-  await Photo.findByIdAndDelete(req.params.id);
-  res.redirect("/");
-});
+app.get("/", getAllPhotos); // route to home page
+app.get("/photos/:id", getPhoto); // route to get photo detail
+app.post("/photos", createPhoto); // add new photo
+app.put("/photos/:id", updatePhoto); // update photo
+app.delete("/photos/:id", deletePhoto); // delete photo
+app.get("/about", getAboutPage); // route to about page
+app.get("/add", getAddPage); // route to add page
+app.get("/photos/edit/:id", getEditPage); // route to edit photo
 
 mongoose.set("strictQuery", false);
 mongoose.connect(
---
+++ controllers/photoControllers.js
@@ -0,0 +1,86 @@
+const Photo = require("../models/Photo");
+
+const fs = require("fs");
+
+exports.getAllPhotos = async (req, res) => {
+  const Photos = await Photo.find({}).sort("-dateCreated");
+  res.render("index", { Photos, page_name: "index" });
+  res.status(200);
+};
+
+exports.getPhoto = async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render("photo-detail", { photo, page_name: "photo_detail" });
+};
+
+exports.createPhoto = async (req, res) => {
+  const uploadDir = "public/uploads";
+  let uploadImage = req.files;
+
+  // if there is no uploads folder, create one
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  // if there is no picture uploaded
+  if (uploadImage === null) {
+    await Photo.create({
+      ...req.body
+    });
+    res.redirect("/");
+  } else {
+    let uploadPath = path.join(
+      __dirname,
+      "public/uploads",
+      uploadImage.image.name
+    );
+
+    uploadImage.image.mv(uploadPath, async () => {
+      await Photo.create({
+        ...req.body,
+        image: `/uploads/${uploadImage.image.name}`
+      });
+    });
+    res.redirect("/");
+  }
+};
+
+exports.updatePhoto = async (req, res) => {
+  let uploadedImage = req.files; // you can use req.files.file.mv() to save file
+  const photo = await Photo.findOne({ _id: req.params.id });
+
+  // if there is no picture uploaded
+  if (uploadedImage === null) {
+    photo.title = req.body.title;
+    photo.description = req.body.description;
+    await photo.save();
+    res.redirect(`/photos/${photo._id}`);
+  } else {
+    let uploadPath = path.join(
+      __dirname,
+      "public/uploads",
+      uploadedImage.image.name
+    );
+
+    uploadedImage.image.mv(uploadPath, async () => {
+      photo.title = req.body.title;
+      photo.description = req.body.description;
+      photo.image = `/uploads/${uploadedImage.image.name}`;
+      await photo.save();
+    });
+    if (fs.existsSync(`public/${photo.image}`)) {
+      fs.unlinkSync(`public/${photo.image}`);
+    }
+    res.redirect(`/photos/${photo._id}`);
+  }
+};
+
+exports.deletePhoto = async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+
+  if (fs.existsSync(`public/${photo.image}`)) {
+    fs.unlinkSync(`public/${photo.image}`);
+  }
+  await Photo.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+};
